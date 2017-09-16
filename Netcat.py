dd4a0fcbb3649007740652646ed2eb584f7bde5d
Adam Galauner <adam.galauner@yahoo.com>
2017-09-16 14:04:37 -0400

    Update Netcat.py

--- Netcat.py
+++ Netcat.py
@@ -41,31 +41,5 @@ class Netcat:
 
 
 
-# start a new Netcat() instance
-'''
-nc = Netcat('misc.chal.csaw.io', 4239)
-import time
 
-ii = 0
-ss = ''
-while (True):
-    s = nc.read(11)
-    ii += 1
-    s0 = s[0:9]
-    print(s0)
-    i = 0
-    for e in range(len(s0)):
-        if s0[e] == '1':
-            i += 1
-    if i == 0 and ii > 15:
-        print(ss)
-        break
-    elif i%2 == 0:
-        ss += '1'
-        nc.write('1')
-    else:
-        ss += '0'
-        nc.write('0')
-    nc.read(1)
-'''
 
